37891d94a0e4ed503a295d85e9d849f86cacbff0
Matheus Santos <78329418+darkmathew@users.noreply.github.com>
2022-03-07 15:55:08 -0300

    add files

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask import Flask
+
 __version__ = '0.1.0'
+
+def _check_cfg_args(config):
+    assert config['SECRET_KEY'] is not None, (
+        "APP SECRET_KEY is not defined"
+    )
+
+
+def create_app(app_configuration):
+    app = Flask(__name__)
+    app.config.from_object(app_config)
+    _check_cfg_args(app_config)
